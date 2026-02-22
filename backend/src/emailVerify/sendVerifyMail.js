a482bdec91c6ba11fd76fb2bf0d0a811c942a6da
AkramMondal2 <abcmondalxyz@gmail.com>
2026-02-22 23:25:57 +0530

    modify send mail

--- backend/src/emailVerify/sendVerifyMail.js
+++ backend/src/emailVerify/sendVerifyMail.js
@@ -3,9 +3,7 @@ import nodemailer from "nodemailer";
 export const sendVerifyMail = async (token, email) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
