9f5cf735d9968f17efa72597e9fcac5bfd78af2f
AkramMondal2 <abcmondalxyz@gmail.com>
2026-02-24 19:11:58 +0530

    modify dbConnect file

--- backend/src/config/dbConnect.js
+++ backend/src/config/dbConnect.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
 export async function dbConnect() {
-    try {
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log("Mongodb connected")
-    } catch (error) {
-        console.log("Mongodb not connected", error)
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "taskManagement",
+    });
+    console.log("Mongodb connected");
+  } catch (error) {
+    console.log("Mongodb not connected", error);
+  }
 }
